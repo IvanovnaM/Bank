66db5452b8e1a73d5f98d9f0603f2bf2217be6f5
IvanovnaM <lungrenok69@gmail.com>
2024-07-28 17:01:40 +0300

    version 2.1

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO account (id, account_number, balance, beneficiar_name, pin_code) VALUES
-                                                                                (1, '1234567890', 7770.00, 'Natalia Sazankova', '1234'),
-                                                                                (2, '0987654321', 6000.00, 'Zoya Bublik', '5678');
-
-INSERT INTO transaction (id, account_id, amount, type, timestamp) VALUES
-                                                                      (1, 1, 7770.00, 'DEPOSIT', NOW()),
-                                                                      (2, 2, 6000.00, 'DEPOSIT', NOW());
